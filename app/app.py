fdf3d238d2fb5ba7a04e8a2d7dbab2dfade2731d
Mahmoud Zangri <hmode.z.2015@gmail.com>
2026-04-10 01:12:25 +0300

    ADD /about endpoint

--- app/app.py
+++ app/app.py
@@ -10,5 +10,13 @@ def home():
 def health():
     return jsonify({"status": "healthy"})
 
+@app.route("/about")
+def about():
+    return jsonify({
+        "author": "Mahmoud Zangri",
+        "project": "CI/CD Pipeline",
+        "stack": ["Python", "Flask", "Docker", "GitHub Actions"]
+    })
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
